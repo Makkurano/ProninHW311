05cdaf899e7f7f29c7b78365ea9ceed016ba1ce5
Makkurano <makkurano@gmail.com>
2019-05-29 15:33:45 +0300

    test

--- app/src/main/java/com/example/proninhw311/MainActivity.java
+++ app/src/main/java/com/example/proninhw311/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.proninhw311;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,61 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button0 = findViewById(R.id.btn_0);
+        Button button1 = findViewById(R.id.btn_1);
+        Button button2 = findViewById(R.id.btn_2);
+        Button button3 = findViewById(R.id.btn_3);
+        Button button4 = findViewById(R.id.btn_4);
+        Button button5 = findViewById(R.id.btn_5);
+        Button button6 = findViewById(R.id.btn_6);
+        Button button7 = findViewById(R.id.btn_7);
+        Button button8 = findViewById(R.id.btn_8);
+        Button button9 = findViewById(R.id.btn_9);
+        Button buttonminus = findViewById(R.id.btn_minus);
+        Button buttonplus = findViewById(R.id.btn_plus);
+        Button buttonmult = findViewById(R.id.btn_multiplication);
+        Button buttondev = findViewById(R.id.btn_division);
+        Button buttoneq = findViewById(R.id.btn_equals);
     }
+
+    private View.OnClickListener mClickListener = new View.OnClickListener() {
+        public void onClick(View v) {
+
+            switch (v.getId()) {
+                case R.id.btn_0:
+                    break;
+                case R.id.btn_1:
+                    break;
+                case R.id.btn_2:
+                    break;
+                case R.id.btn_3:
+                    break;
+                case R.id.btn_4:
+                    break;
+                case R.id.btn_5:
+                    break;
+                case R.id.btn_6:
+                    break;
+                case R.id.btn_7:
+                    break;
+                case R.id.btn_8:
+                    break;
+                case R.id.btn_9:
+                    break;
+                case R.id.btn_minus:
+                    break;
+                case R.id.btn_plus:
+                    break;
+                case R.id.btn_multiplication:
+                    break;
+                case R.id.btn_division:
+                    break;
+                case R.id.btn_equals:
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
 }
